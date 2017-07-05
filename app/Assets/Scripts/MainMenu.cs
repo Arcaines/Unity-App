fcd6bf7170c1e623bd46eac4c112470426c8f727
Arcaines <markusdomagala@gmx.de>
2017-07-05 19:29:25 +0200

    First commit
    add to github

---
+++ app/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour 
+{
+    public string scenename;
+
+	public void LookAtMenu(Transform menuTransform)
+	{
+		Camera.main.transform.LookAt (menuTransform.position);
+	}
+
+    public void ChangeScene(string scenename)
+    {
+        SceneManager.LoadScene(scenename);
+    }
+}
